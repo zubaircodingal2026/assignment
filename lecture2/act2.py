0b4c4275ac0d8aa8d0071a614d5dc7ccf6bccfd9
zubaircodingal2026 <jewelrani2005@gmail.com>
2026-04-13 20:36:59 +0600

    updates

--- lecture2/act2.py
+++ lecture2/act2.py
@@ -8,5 +8,4 @@ print("Addition: ",num1 + num2)
 print("Subtraction: ",num1 - num2)
 print("Multiplication: ",num1 * num2)
 print("Division: ",num1 / num2)
-print("Division: ",num1  num2)
-
+print("Division: ",num1 ** num2)
--- lecture3/act1.py
+++ lecture3/act1.py
@@ -1,10 +1,10 @@
-tree1 = 98 
-tree2 = 94
-tree3 = 41 
-tree4 = 95
-tree5 = 11
+tree1  = 98
+tree2  = 97
+tree3  = 99
+tree4  = 94
+tree5  = 96 
 
-sum    = tree1 + tree2 + tree3 + tree4 +tree5
+sum    = (tree1 + tree2 + tree3 + tree4 + tree5)
 print(f"sum of all trees:{sum}")
 
 average = sum / 5
