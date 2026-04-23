be342e83a2a379ee8bcc1f411efa0fe4bb44f1bb
zubaircodingal2026 <jewelrani2005@gmail.com>
2026-04-23 23:21:46 +0600

    updates

---
+++ assignments/assignment8.py
@@ -0,0 +1,13 @@
+a =int(input("enter the first number : "))
+b =int(input("enter the second number : "))
+c =int(input("enter the third number : "))
+
+#swapping (rotional values)
+("a,b,c = b,c,a")
+
+#values after swapping
+print("after swaping values")
+print("a =", b)
+print("b =", c)
+print("c =", a)
+
