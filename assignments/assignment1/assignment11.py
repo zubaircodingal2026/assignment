b68dc12b7c3e31c6e3d6b5e00dccbe4778c05c46
zubaircodingal2026 <jewelrani2005@gmail.com>
2026-06-11 21:44:30 +0600

    updates

--- assignments/assignment1/assignment11.py
+++ assignments/assignment1/assignment11.py
@@ -1,7 +1,11 @@
-# Convert decimal to binary
+age = [10,20,30,40,50,60,70,80,90,100]
 
-decimal = int(input("Enter a decimal number: "))
-
-binary = bin(decimal)
-
-print("Binary number is:", binary[2:])
+try:
+    index = int(input("Enter age : "))
+    print("value =",age[index])
+except IndexError:
+    print("error:age is out of range.")
+except ValueError:
+    print("Error:please enter a valid age.")
+else:
+    print("element accessed successfully.")
