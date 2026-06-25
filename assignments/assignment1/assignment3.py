99744cb9e54c2c85c311fc535557ac3cc5b8df59
zubaircodingal2026 <jewelrani2005@gmail.com>
2026-06-25 21:33:54 +0600

    updates

--- assignments/assignment1/assignment3.py
+++ assignments/assignment1/assignment3.py
@@ -1,7 +1,29 @@
-num = int(input("enter your num : "))
+def square_filter(start, end):
+    even_squares = []
+    odd_squares = []
 
-if num % 9 == 0:
-    print("even")
+    print("Square Values:")
 
-else : 
-    print("odd")
+    for num in range(start, end + 1):
+        square = num ** 2
+        print(f"{num}^2 = {square}")
+
+        if square % 2 == 0:
+            even_squares.append(square)
+        else:
+            odd_squares.append(square)
+
+    print("\nEven Square Values:")
+    for value in even_squares:
+        print(value)
+
+    print("\nOdd Square Values:")
+    for value in odd_squares:
+        print(value)
+
+# Taking input from the user
+begin = int(input("Enter the beginning number: "))
+end = int(input("Enter the ending number: "))
+
+# Calling the function
+square_filter(begin, end)
